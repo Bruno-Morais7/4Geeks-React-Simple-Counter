e228db8908b3d3c28a6767ca244408001b176cc3
Bruno-Morais7 <brunomafonsomorais@gmail.com>
2022-04-11 11:33:02 +0000

    Cleaned files

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-//import Home from "./component/home.jsx";
 import Counter from "./component/counter.jsx";
 
 //render your react application
